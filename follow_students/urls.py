8227811ed01ffc3c3849d2093576480e5ad8a45d
DiegoZM1006 <1109184718@u.icesi.edu.co>
2023-09-28 21:27:24 -0500

    feat(signin/layout): Adding the signin and the layout that contains the sidebar and navbar

--- follow_students/urls.py
+++ follow_students/urls.py
@@ -1,8 +1,11 @@
+# from follow_students.views.(name) import (name) || Here the views are added from the views folder
 from django.urls import path
 from django.contrib import admin
-# from follow_students.views.(name) import (name) || Here the views are added from the views folder
+from follow_students.views.signin import Signin
+from follow_students.views.dashboard import Dashboard
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     # Here the paths are added
+    path('', Signin.as_view()),
+    path('dashboard', Dashboard.as_view())
 ]
