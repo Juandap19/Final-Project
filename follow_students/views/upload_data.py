2d79e6423969536a9ef18451298704d4ab257f38
darwinl-06 <darwinlenis.06@gmail.com>
2023-09-30 15:36:53 -0500

    feat(load-data):shows info when uploading a file

--- follow_students/views/upload_data.py
+++ follow_students/views/upload_data.py
@@ -11,7 +11,8 @@ from django.views import View
 class Load_data(View):
     def get(self, request):
         form = UploadFileForm()
-        return render(request, 'upload_data.html',  {'form': form})
+        return render(request, 'upload_data.html', {
+            'form': form})
     
     def post(self, request):
         if request.method == 'POST':
@@ -29,14 +30,15 @@ class Load_data(View):
                                 curso=curso,
                                 defaults={'grade': row['NotaCurso']}
                             )
-                        except Exception as e:
+                        except Exception as e:                        
                             messages.error(request, f'Hubo un error al cargar el archivo')
-                            return HttpResponseRedirect(request.path)
+                            return HttpResponseRedirect(request.path)                      
                 messages.success(request, 'Se cargo correctamente')
                 return HttpResponseRedirect(request.path)
         else:
             form = UploadFileForm()
-            return render(request, 'load_page.html', {'form': form})
+            return render(request, 'load_page.html', {
+                'form': form})
 
 
 
