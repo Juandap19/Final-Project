80174c39f2f19aaecdb5cc9276b357cc09e80663
DiegoZM1006 <1109184718@u.icesi.edu.co>
2023-10-01 17:09:07 -0500

    style(feat-load-data/program-director): Add CSS and Bootstrap styles for improved UI

--- follow_students/views/upload_data.py
+++ follow_students/views/upload_data.py
@@ -37,7 +37,7 @@ class Load_data(View):
                 return HttpResponseRedirect(request.path)
         else:
             form = UploadFileForm()
-            return render(request, 'upload_page.html', {
+            return render(request, 'upload_data.html', {
                 'form': form})
 
 
