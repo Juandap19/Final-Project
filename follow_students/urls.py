e479d4becb161f7f815fb67bc601c7c96ea4a6a5
Juandap19 <juanda232405@hotmail.com>
2023-10-05 11:58:46 -0500

    refactor: :bug: UNIQUE constraint bug fixed
    The UNIQUE constraint Bug was fixed, in addition to adding options in the html to then make payment screens for food and transportation of a scholarship
    The UNIQUE constraint Bug was fixed, in addition to adding options in the html to then make payment screens for food and transportation of a scholarship

--- follow_students/urls.py
+++ follow_students/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     # Here the paths are added
     path('', Signin.as_view()),
     path('dashboard', Dashboard.as_view()),
-    path('apoyo_financiero', FinancialSupport.as_view() ),
+    path('apoyo_financiero_alimentacion', FinancialSupport.as_view() ),
 ]
--- follow_students/views/financial.py
+++ follow_students/views/financial.py
@@ -14,7 +14,7 @@ class FinancialSupport(View):
             'error': False
         })
     def post(self, request):
-        code =random.randint(0,250)
+        code =random.randint(0,50000000000)
         student_code = request.POST['student_code']
         money_quantity = request.POST['money_quantity']
         acumulate_time = request.POST['acumulate_time']
@@ -22,14 +22,25 @@ class FinancialSupport(View):
         query = Student.objects.filter(code = student_code) # search the student
         if query:
             query = Student.objects.get(code = student_code)
-            expense = Scholarship_Expenses.objects.create(code = code , student_code = student_code, money_quantity = money_quantity, acumulate_time=  acumulate_time, select_time = select_time)  #Create a expense to One particular Student and save it
-            expense.save()
             query1 = Scholarship.objects.get(code = query.scholarship_id.code)  #Search the scholarship that is associated with the student 
             new_value = float(query1.code_Funds.alimentation_fund) - float(money_quantity)  #Calculate the new value
-            query1.code_Funds.alimentation_fund = new_value
-            query1.code_Funds.save()    #Save the new value in the data base
-            messages.success(request,"Proceso completado")
-            return HttpResponseRedirect(request.path)   
+            if(new_value > 0):
+                query1.code_Funds.alimentation_fund = new_value
+                query1.code_Funds.save() 
+                expense = Scholarship_Expenses.objects.create(code = code , student_code = student_code, money_quantity = money_quantity, acumulate_time=  acumulate_time, select_time = select_time)  #Create a expense to One particular Student and save it
+                expense.save()   #Save the new value in the data base
+                messages.success(request,"Proceso completado")
+                return HttpResponseRedirect(request.path)   
+            else:
+                #Case when the Scholarship doesn`t have enought money to pay the new student money quantity`
+                money_quantity = query1.code_Funds.alimentation_fund
+                query1.code_Funds.alimentation_fund = 0
+                query1.code_Funds.save()  
+                expense = Scholarship_Expenses.objects.create(code = code , student_code = student_code, money_quantity = money_quantity, acumulate_time=  acumulate_time, select_time = select_time)  #Create a expense to One particular Student and save it
+                expense.save()
+                new_value = abs(new_value)
+                messages.warning(request,"Se acabo el monto de la beca falta por pagar: {}".format(new_value))
+                return  HttpResponseRedirect(request.path)   
         else:
             messages.error(request,"El codigo "+ student_code + " no existe")
             return HttpResponseRedirect(request.path)
