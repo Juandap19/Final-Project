3e2130d9d8ccd048f4e9449440477e280cf3b4b4
DiegoZM1006 <1109184718@u.icesi.edu.co>
2023-10-07 21:13:51 -0500

    feat(student-manage): add student information editing functionality

--- follow_students/views/student_edit.py
+++ follow_students/views/student_edit.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.views import View
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.exceptions import ObjectDoesNotExist
@@ -14,4 +15,23 @@ class StudentEdit(View):
     
     def post(self, request, *args, **kwargs):
 
-        return redirect(request, 'studentManage.html')
+        id = int(request.POST['idStudent'])
+
+        student_to_edit = Students.objects.get(pk = id)
+
+        student_to_edit.code = request.POST['code']
+        student_to_edit.name = request.POST['name']
+        student_to_edit.id_card = request.POST['id_card']
+        student_to_edit.phone_number = request.POST['phone_number']
+        date_str = request.POST['birthday']
+        date_obj = datetime.strptime(date_str, '%Y-%m-%d').date()
+        student_to_edit.date = date_obj
+        student_to_edit.icfes = request.POST['icfes']
+        student_to_edit.mail = request.POST['mail']
+
+        student_to_edit.save()
+
+        return render(request, 'studentEdit.html', {
+            "student": student_to_edit,
+            "studentUpdate": True
+        })
