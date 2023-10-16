d9e7cf06bd3c9241cea295762bdec7647aeb5ff0
Juandap19 <juanda232405@hotmail.com>
2023-10-16 17:11:19 -0500

    fix: update

--- follow_students/models.py
+++ follow_students/models.py
@@ -50,7 +50,7 @@ class Donante(models.Model):
     
 
 class Montos(models.Model):
-    codigo = models.CharField(max_length=20, unique=True)
+    codigo = models.CharField(max_length=20, unique=True, default = "23")
     transporte = models.IntegerField()
     alimentacion = models.IntegerField()
     academico = models.IntegerField()
