d3c7b8e84067a0c3ca4e69d19ac0bf81592e0e5f
DiegoZM1006 <1109184718@u.icesi.edu.co>
2023-10-17 00:40:04 -0500

    fix(styles): fix styles on upload data screens

--- follow_students/forms/upload_dataCREA_form.py
+++ follow_students/forms/upload_dataCREA_form.py
@@ -4,5 +4,9 @@ class RegistroConsulta(forms.Form):
     codigo_estudiante = forms.CharField(max_length=10)
     fecha = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
     hora = forms.TimeField(widget=forms.TimeInput(attrs={'type': 'time'}))
-    motivo = forms.CharField(widget=forms.Textarea, max_length=200)
-    resultado = forms.CharField(widget=forms.Textarea, max_length=200)
+    motivo = forms.CharField(
+        widget=forms.Textarea(attrs={'cols': 30, 'rows': 5, 'style': 'resize: none;'})
+    )
+    resultado = forms.CharField(
+        widget=forms.Textarea(attrs={'cols': 30, 'rows': 5, 'style': 'resize: none;'})
+    )
