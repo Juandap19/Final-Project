476ce4661cca95a222cae5b9b44a687eae78fb70
darwinl-06 <darwinlenis.06@gmail.com>
2023-11-12 12:35:45 -0500

    style: translates upload data CREA code

--- follow_students/forms/upload_dataCREA_form.py
+++ follow_students/forms/upload_dataCREA_form.py
@@ -1,12 +1,12 @@
 from django import forms
 
-class RegistroConsulta(forms.Form):
-    codigo_student = forms.CharField(max_length=10)
-    fecha = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
-    hora = forms.TimeField(widget=forms.TimeInput(attrs={'type': 'time'}))
-    motivo = forms.CharField(
+class RegisConsult(forms.Form):
+    student_code = forms.CharField(max_length=10)
+    date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+    time = forms.TimeField(widget=forms.TimeInput(attrs={'type': 'time'}))
+    reason = forms.CharField(
         widget=forms.Textarea(attrs={'cols': 30, 'rows': 5, 'style': 'resize: none;'})
     )
-    resultado = forms.CharField(
+    outcome = forms.CharField(
         widget=forms.Textarea(attrs={'cols': 30, 'rows': 5, 'style': 'resize: none;'})
     )
