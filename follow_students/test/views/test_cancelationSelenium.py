c9a78d1c1885b0adab8577e0ee3533c307c635a7
Juan <1017922242@u.icesi.edu.co>
2023-11-15 18:44:32 -0500

    feat:adding test

--- follow_students/test/views/test_cancelationSelenium.py
+++ follow_students/test/views/test_cancelationSelenium.py
@@ -14,9 +14,25 @@ class testCancelation(LiveServerTestCase):
 
       self.driver = webdriver.Chrome()
 
-      self.driver.get('http://127.0.0.1:8000/menuCancelation/')
+      self.driver.get('http://127.0.0.1:8000')
 
   def testCancelationCourse(self):
+
+    user = self.driver.find_element(by=By.ID,value='id_username')
+    password = self.driver.find_element(by=By.ID,value='id_password')
+   
+    submit = self.driver.find_element(by=By.ID, value='inicio')
+
+    #populate the form with data
+    user.send_keys('2')
+    password.send_keys('2')
+
+    #submit form
+    submit.send_keys(Keys.RETURN)
+
+
+    submit = self.driver.find_element(by=By.ID, value='cancelacion')
+    submit.send_keys(Keys.RETURN)
 
     submit = self.driver.find_element(by=By.ID, value='selection')
   
